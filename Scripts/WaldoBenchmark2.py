e4b5625088065ffa4a50156e96302ba19b68b84a
thomacdebabo <thomas.rueegg98@bluewin.ch>
2018-05-21 13:26:12 +0200

    updated files, imporved Dataset

--- Scripts/WaldoBenchmark2.py
+++ Scripts/WaldoBenchmark2.py
@@ -1,25 +1,42 @@
+#This script will use a pretrained Model to find Waldo.
+#The best Matches are shown in red, and all other matches over the threshold will be in blue to black where blue is higher and black lower confidence.
+
 import cv2
 import numpy as np
 import time
-
 from keras.models import load_model
 start = time.time()
-DataDir = r"C:\Users\Thomacdebabo\Downloads\Dataset(1)\Dataset\Controll\2.jpg"
-ModelName = "waldoBig5.hdf5"
+
+
+#Directories and ModelNames. Model has to be in the same folder as this script
+DataDir = r"F:\WaldoBilder\6.jpg"
+ModelName = "waldoNewDataset.hdf5"
+
+#Imagesize required by your model
 ImgSize = 64
-max = 100
-thresh = 0.8
 
-psf = 0.75
+#stepsize of the search window
+stepsize = 32
 
+#Threshold for searching Algorithm
+thresh = 0.9
+
+#Number of top matches seen in red
+max = 10
+
+#pre-scaling factor
+psf = 0.9
+
+#load model
 model = load_model(ModelName)
 
+#preprocess image
 img = cv2.imread(DataDir, 1)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img = cv2.resize(img, (0, 0), fx=psf, fy=psf, interpolation=cv2.INTER_CUBIC)
 a = img.shape
 
-stepsize = 32
+
 
 xit = int((a[0]-ImgSize)/stepsize)
 yit = int((a[1]-ImgSize)/stepsize)
@@ -30,7 +47,7 @@ for x in range(xit):
         pic1 = img[x*stepsize:x*stepsize+ImgSize,y*stepsize:y*stepsize+ImgSize]
         pic = np.array([pic1])
         pred[x,y]= model.predict(pic/255.)
-        print model.predict(pic/255.)
+        print (model.predict(pic/255.))
 
 
  
@@ -39,20 +56,25 @@ while pred[np.unravel_index(np.argmax(pred, axis=None), pred.shape)]>thresh:
     index = np.unravel_index(np.argmax(pred, axis=None), pred.shape)
     indexarr = np.asarray(index)
     
-    print pred[index]
+    print (pred[index])
     pred[index]= 0.
     
     pt1 = indexarr*stepsize
     pt2 = indexarr*stepsize+[ImgSize,ImgSize]
     pt1 = (pt1[1],pt1[0])
     pt2 = (pt2[1],pt2[0])
-    cv2.rectangle(img,pt1,pt2, (255-z,0,z),5)
+    if z < max:
+        cv2.rectangle(img,pt1,pt2, (255,0,0),5)
+    else:
+        cv2.rectangle(img,pt1,pt2, (0,0,255-z,255-z),5)
     z = z+1
-print z
+print ("Number of matches found: "+ str(z))
 end = time.time()
 print ("time elapsed: " + str(end-start)+" seconds")
 
-img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+scale = 0.5
+
+img = cv2.resize(img, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 cv2.imshow("Wo isch de Walti?", img)
 cv2.waitKey(0)
